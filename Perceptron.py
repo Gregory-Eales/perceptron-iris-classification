4c1a781392bebf8deb09accae7f0a2119f9fb0d3
Gregory Eales <33496134+Gregory-Eales@users.noreply.github.com>
2018-02-27 08:49:43 -0800

    Create Perceptron.py

---
+++ Perceptron.py
@@ -0,0 +1,73 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+
+class Perceptron(object):
+    def __init__(self, Learn_Rate=0.5, Iterations=10):
+        self.learn_rate = Learn_Rate
+        self.Iterations = Iterations
+        self.errors = []
+        self.weights = np.zeros(1 + x.shape[1])
+
+    def fit(self, x, y):
+        self.weights = np.zeros(1 + x.shape[1])
+        for i in range(self.Iterations):
+            error = 0
+            for xi, target in zip(x, y):
+                update = self.learn_rate * (target - self.predict(xi))
+                self.weights[1:] += update*xi
+                self.weights[0] += update
+                error += int(update != 0)
+            self.errors.append(error)
+        return self
+
+    def net_input(self, x):
+        return np.dot(x, self.weights[1:]) + self.weights[0]
+
+    def predict(self, x):
+        return np.where(self.net_input(x) >= 0.0, 1, -1)
+
+
+y = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header=None)
+x = y.iloc[0:100, [0, 2]].values
+plt.scatter(x[:50, 0], x[:50, 1], color='red')
+plt.scatter(x[50:100, 0], x[50:100, 1], color='blue')
+plt.scatter(x[100:150, 0], x[100:150, 1], color='yellow')
+plt.show()
+y = y.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+
+Classifier = Perceptron(Learn_Rate=0.01, Iterations=50)
+Classifier.fit(x, y)
+plt.plot(range(1, len(Classifier.errors) + 1), Classifier.errors, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
+plt.show()
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+plot_decision_regions(x, y, classifier=Classifier)
+plt.show()
